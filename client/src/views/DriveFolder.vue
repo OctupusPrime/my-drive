cde4d78d439a0080ba27f4c1654b55fa72b05822
Octupus Prime <rasiapypkin2014@gmail.com>
2021-09-13 13:41:26 +0300

    working folders

--- client/src/views/DriveFolder.vue
+++ client/src/views/DriveFolder.vue
@@ -1,31 +1,45 @@
 <template>
-    <div @click="undoSelect" style="min-height: 70vh">
+    <div style="min-height: 70vh"
+        @click="undoSelect($event)">
         <FolderItemsList 
             :itemsArr="folder.children" 
-            @dbClickCallback="redirectToFile"
+            @clickCallback="selectFolder"
+            @dbClickCallback="openFolder"
             v-if="folder.children.length">
             Folders
         </FolderItemsList>
 
         <FolderItemsList 
             :itemsArr="folder.data"
+            icon="mdi-file"
+            @clickCallback="selectFile"
+            @dbClickCallback="openFile"
             v-if="folder.data.length">
             Files
         </FolderItemsList>
+
+        <FileOpened ref="openedFile"></FileOpened>
     </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import FolderItemsList from '../components/FolderItemsList.vue'
+import FileOpened from '../components/FileOpened.vue'
+
 
 export default {
     name: 'DriveFolder',
     components: {
-        FolderItemsList
+        FolderItemsList,
+        FileOpened
     },
+    data: () => ({
+        isShiftPressed: false,
+        quickMenuItems: []
+    }),
     computed: {
-        ...mapGetters(['getFolder']),
+        ...mapGetters(['getFolder', 'selectedFiles']),
         folder() {
             let obj = this.getFolder(this.$route.params.id)
             this.storeSetCurrentBranch(obj)
@@ -33,14 +47,51 @@ export default {
         }
     },
     methods: {
-        ...mapActions(['storeSetCurrentBranch']),
-        redirectToFile(item) {
+        ...mapActions([
+            'storeSetCurrentBranch', 
+            'storeSelectFile', 
+            'storeClearSelectFile'
+        ]),
+        selectFolder(index) {
+            if (this.isShiftPressed) {
+                this.storeSelectFile({type: 'folderIndex', index, multiple: true})
+            }
+            else {
+                this.storeClearSelectFile()
+                this.storeSelectFile({type: 'folderIndex', index})  
+            }
+        },
+        openFolder(item) {
+            this.storeClearSelectFile()
             const { id } = item
             this.$router.push({ name: 'Folder', params: { id } })
         },
-        undoSelect() {
-            console.log(1)
+        selectFile(index) {
+            if (this.isShiftPressed) {
+                this.storeSelectFile({type: 'fileIndex', index, multiple: true})
+            }
+            else {
+                this.storeClearSelectFile()
+                this.storeSelectFile({type: 'fileIndex', index})  
+            }            
+        },
+        openFile(item) {
+            this.$refs.openedFile.open(item)
+        },
+        undoSelect(e) {
+            const btn = e.target.closest('button')
+            if (!btn) {
+                this.storeClearSelectFile()
+            }
         }
+    },
+    mounted() {
+        window.addEventListener('keyup', (e) => {
+            this.isShiftPressed = e.shiftKey
+        })
+        window.addEventListener('keydown', (e) => {
+            this.isShiftPressed = e.shiftKey
+        })
     }
 }
 </script>
