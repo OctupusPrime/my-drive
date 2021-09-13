b4f722c84b317b3c78a1c4b9ae3c8b4b64cf2d6f
Octupus Prime <rasiapypkin2014@gmail.com>
2021-09-14 00:36:35 +0300

    working server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import "dotenv/config"
 import express from 'express'
+import cors from 'cors';
 
-const app = express(),
-    drive = '1'
+import {data} from './data.js'
 
-app.get('/', (req, res) => {
-    return res.send({data: drive})
+const app = express()
+
+app.use(cors())
+
+app.get('/api/drive', (req, res) => {
+    setTimeout(() => {
+        return res.send({data})   
+    }, 3000)
 })
 
 app.listen(process.env.APP_PORT, () => {
